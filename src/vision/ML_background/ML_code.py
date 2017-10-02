301a54cad02ca2a814b7295b2df11200fab37347
matthijs <vissermatthijs@hotmail.nl>
2017-10-02 15:51:16 +0200

    machine learning forground background substraction added

--- src/vision/ML_background/ML_code.py
+++ src/vision/ML_background/ML_code.py
@@ -5,3 +5,4 @@ img, path, filename = pcv.readimage("yucca_1.jpg")
 
 # Classify the pixels as plant or background
 device, mask = pcv.naive_bayes_classifier(img, pdf_file="naive_bayes_pdfs.txt", device=0, debug="plot")
+
--- src/vision/scripts/feature_extract.py
+++ src/vision/scripts/feature_extract.py
@@ -8,8 +8,8 @@ import plantcv as pcv
 def options():
     parser = argparse.ArgumentParser(description="Imaging processing with opencv")
     parser.add_argument("-i", "--image", help="Input image file.", required=True)
-    parser.add_argument("-o", "--outdir", help="Output directory for image files.", required=False)
-    parser.add_argument("-r", "--result", help="result file.", required=False)
+    parser.add_argument("-o", "--outdir", help="Output directory for image files.", required=True)
+    parser.add_argument("-r", "--result", help="result file.", required=True)
     parser.add_argument("-w", "--writeimg", help="write out images.", default=False)
     parser.add_argument("-D", "--debug", help="Turn on debug, prints intermediate images.", default=None)
     args = parser.parse_args()
@@ -23,24 +23,24 @@ def main():
     debug = args.debug
     # Read image
     img, path, filename = pcv.readimage(args.image)
-
     # Pipeline step
     device = 0
-
+    device, resize_img = pcv.resize(img, 0.2, 0.2, device, debug)
     # Classify the pixels as plant or background
-    device, mask = pcv.naive_bayes_classifier(img, pdf_file="naive_bayes_pdfs.txt", device=0, debug="plot")
+    device, mask = pcv.naive_bayes_classifier(resize_img, pdf_file="naive_bayes_pdfs.txt", device=0, debug=False)
 
     # Identify objects
-    device, id_objects, obj_hierarchy = pcv.find_objects(img, mask, device, debug)
+    device, id_objects, obj_hierarchy = pcv.find_objects(resize_img, mask.get('plant'), device, debug)
 
     # Define ROI
-    device, roi1, roi_hierarchy = pcv.define_roi(mask, 'rectangle', device, None, 'default', debug, True, 550, 0,
-                                                 -500, -1900)
+    device, roi1, roi_hierarchy = pcv.define_roi(mask.get('plant'), 'rectangle', device, roi=None, roi_input='default',
+                                                 debug=True, adjust=False, x_adj=100, y_adj=50, w_adj=-150,
+                                                 h_adj=-50)
     # Decide which objects to keep
-    device, roi_objects, hierarchy3, kept_mask, obj_area = pcv.roi_objects(img, 'partial', roi1, roi_hierarchy,
+    device, roi_objects, hierarchy3, kept_mask, obj_area = pcv.roi_objects(resize_img, 'partial', roi1, roi_hierarchy,
                                                                            id_objects, obj_hierarchy, device, debug)
     # Object combine kept objects
-    device, obj, mask = pcv.object_composition(img, roi_objects, hierarchy3, device, debug)
+    device, obj, mask = pcv.object_composition(resize_img, roi_objects, hierarchy3, device, debug)
 
     ############### Analysis ################
 
@@ -49,15 +49,17 @@ def main():
         outfile = args.outdir + "/" + filename
 
     # Find shape properties, output shape image (optional)
-    device, shape_header, shape_data, shape_img = pcv.analyze_object(img, args.image, obj, mask, device, debug,
+    device, shape_header, shape_data, shape_img = pcv.analyze_object(resize_img, args.image, obj, mask, device, debug,
                                                                      args.outdir + '/' + filename)
 
     # Shape properties relative to user boundary line (optional)
-    device, boundary_header, boundary_data, boundary_img1 = pcv.analyze_bound(img, args.image, obj, mask, 1680, device,
+    device, boundary_header, boundary_data, boundary_img1 = pcv.analyze_bound(resize_img, args.image, obj, mask, 1680,
+                                                                              device,
                                                                               debug, args.outdir + '/' + filename)
 
     # Determine color properties: Histograms, Color Slices and Pseudocolored Images, output color analyzed images (optional)
-    device, color_header, color_data, color_img = pcv.analyze_color(img, args.image, kept_mask, 256, device, debug,
+    device, color_header, color_data, color_img = pcv.analyze_color(resize_img, args.image, kept_mask, 256, device,
+                                                                    debug,
                                                                     'all', 'v', 'img', 300,
                                                                     args.outdir + '/' + filename)
 
