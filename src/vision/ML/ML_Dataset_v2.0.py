8d82e26d0825c1392f422aa1a9f49758dc3e559d
Tim <timypenburg@hotmail.com>
2017-10-10 22:21:46 +0200

    XGBoost machine learning werkt!!

--- src/vision/ML/ML_Dataset_v2.0.py
+++ src/vision/ML/ML_Dataset_v2.0.py
@@ -1,18 +1,12 @@
 # Load libraries
-import pandas as pd
-import xgboost as xgb
-from sklearn.preprocessing import LabelEncoder
-from numpy import loadtxt
-from xgboost import XGBClassifier
 from sklearn.metrics import accuracy_score
+from xgboost import XGBClassifier
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from sklearn.svm import SVC
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.cross_validation import train_test_split
 from sklearn import datasets
 import numpy as np
 import csv
-import pandas as pd
 
 data=[]
 target=[]
@@ -23,11 +17,12 @@ with open('plant_db.csv') as csvfile:
         data.append(row[1:])
         target.append(row[0])
 
-
 data = np.asarray(data)
-#print(data)
 
-X_train, X_test, Y_train, Y_test = train_test_split(data, target, test_size=0.17, random_state=20)
+seed = 20
+test_size = 0.25
+X_train, X_test, Y_train, Y_test = train_test_split(data, target, test_size=test_size, random_state=seed)
+
 
 scaler = MinMaxScaler()
 X_train_norm = scaler.fit_transform(X_train)
@@ -37,14 +32,11 @@ lda = LinearDiscriminantAnalysis(n_components=3)
 X_train_lda = lda.fit_transform(X_train_norm, Y_train)
 X_test_lda = lda.transform(X_test_norm)
 
-
+# fit model no training data
 model = XGBClassifier()
 model.fit(X_train_lda, Y_train)
 
-print(model)
-
+# make predictions for test data
 y_pred = model.predict(X_test_lda)
-
-
 
 print ('accuracy: %0.4f' % accuracy_score(Y_test, y_pred))
--- src/vision/ML/temo.py
+++
@@ -1,5 +0,0 @@
-import csv
-with open('plant_db.csv', newline='') as csvfile:
-    dataset = csv.reader(csvfile, delimiter=' ', quotechar='|')
-    for row in dataset:
-        print(', '.join(row))
