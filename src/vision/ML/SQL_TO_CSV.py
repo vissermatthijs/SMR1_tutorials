512c090270e78627917b9d59dc2e7211919feebf
matthijs <vissermatthijs@hotmail.nl>
2017-10-11 11:35:24 +0200

    added the new data bases

--- src/vision/ML/SQL_TO_CSV.py
+++ src/vision/ML/SQL_TO_CSV.py
@@ -1,8 +1,9 @@
-import sqlite3
 import csv
+import sqlite3
+
 csvWriter = csv.writer(open("plant_db.csv", "w"))
 
-conn = sqlite3.connect('scripts/plant_db_1')
+conn = sqlite3.connect('scripts/plant_db_1_v2')
 c = conn.cursor()
 c.execute('SELECT * from features')
 
@@ -16,8 +17,7 @@ for row in rows:
     # do your stuff
     csvWriter.writerow(rowindex)
 
-
-conn = sqlite3.connect('scripts/plant_db_2')
+conn = sqlite3.connect('scripts/plant_db_2_v2')
 c = conn.cursor()
 c.execute('SELECT * from features')
 
@@ -31,7 +31,7 @@ for row in rows:
     # do your stuff
     csvWriter.writerow(rowindex)
 
-conn = sqlite3.connect('scripts/plant_db_3')
+conn = sqlite3.connect('scripts/plant_db_3_v2')
 c = conn.cursor()
 c.execute('SELECT * from features')
 
